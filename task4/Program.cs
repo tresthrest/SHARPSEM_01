cabd6898f86a7639d532bce272b877dbc89b4978
tresthrest <135151213+tresthrest@users.noreply.github.com>
2023-06-16 00:17:41 +0300

    Задача №5

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+Console.Write("Введите число :");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 0;
+if (numberA > 0) {numberB = -numberA; }
+else {
+        numberB = numberA;
+        numberA= -numberA; 
+}
+while (numberA >= numberB)
+{
+    Console.Write(numberB + " ");
+    numberB++;
+}

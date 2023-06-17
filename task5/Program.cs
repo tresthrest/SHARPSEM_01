27fd91f91b108469d7695051bfb0ddb0376223d9
tresthrest <135151213+tresthrest@users.noreply.github.com>
2023-06-17 11:17:58 +0300

    Задача с друзьями и собакой 2

---
+++ task5/Program.cs
@@ -0,0 +1,27 @@
+﻿double count = 0;
+double distance = 10000;
+double firstFriendSpeed = 1;
+double secondFriendSpeed = 2;
+double dogSpeed = 5;
+double friend = 2;
+double time = 0;
+
+while (distance > 10)
+{
+    if(friend == 1)  
+    {
+        time = distance / (firstFriendSpeed + dogSpeed); 
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed); 
+        friend = 1;
+    }
+distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+count = count + 1;
+Console.WriteLine(distance);
+}
+
+Console.WriteLine("Count = ");
+Console.WriteLine(count);

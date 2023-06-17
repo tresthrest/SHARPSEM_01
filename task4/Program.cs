01d6c6d82add15dd52fc7ed28193072c411a22d7
tresthrest <135151213+tresthrest@users.noreply.github.com>
2023-06-17 11:17:16 +0300

    Задача с друзьями и собакой

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,7 +2,30 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> "-2, -1, 0, 1, 2"
 
-Console.Write("Введите число :");
+// Console.Write("Введите число :");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// int numberB = 0;
+// if (numberA > 0) {numberB = -numberA; }
+// else {
+//         numberB = numberA;
+//         numberA= -numberA; 
+// }
+// while (numberA >= numberB)
+// {
+//     Console.Write(numberB + " ");
+//     numberB++;
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = num; i > -num; i--)
+// {
+//     Console.Write($"{i}, ");
+// }
+// Console.Write(-num);
+
+Console.Write("Введите число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 int numberB = 0;
 if (numberA > 0) {numberB = -numberA; }
